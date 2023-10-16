957d40e1b333e1b2e0d9bbd585b4246468896559
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2023-10-16 13:22:28 -0300

    Update main.c

--- Pratica 4/Pratica 4/main.c
+++ Pratica 4/Pratica 4/main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 int main() {
-    // Abre o arquivo em modo de leitura
     FILE* arquivo = fopen("arquivo.txt", "r");
 
     if (arquivo == NULL) {
@@ -10,17 +9,13 @@ int main() {
         return 1;
     }
 
-    // Lê a string do arquivo
     char str[1000];
     fgets(str, sizeof(str), arquivo);
 
-    // Fecha o arquivo de leitura
     fclose(arquivo);
 
-    // Calcula o comprimento da string
     int comprimento = strlen(str);
 
-    // Abre o arquivo em modo de escrita
     arquivo = fopen("arquivo.txt", "w");
 
     if (arquivo == NULL) {
@@ -28,10 +23,8 @@ int main() {
         return 1;
     }
 
-    // Escreve o comprimento da string no arquivo
     fprintf(arquivo, "Comprimento: %d\n", comprimento);
 
-    // Inverte a string
     int i, j;
     char temp;
     for (i = 0, j = comprimento - 1; i < j; i++, j--) {
@@ -40,10 +33,9 @@ int main() {
         str[j] = temp;
     }
 
-    // Escreve a string invertida no arquivo
     fprintf(arquivo, "String Invertida: %s", str);
 
-    // Fecha o arquivo de escrita
+
     fclose(arquivo);
 
     printf("Comprimento da string: %d\n", comprimento);
